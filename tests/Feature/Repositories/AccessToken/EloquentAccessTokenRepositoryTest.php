a37f72b798c091607208e19fba8d139abb2c8414
Carlos Alexandre <carlos206819@gmail.com>
2025-06-19 10:41:11 -0300

    test(auth): add integration tests for AccessTokenRepository
    Adds a suite of integration tests for the `EloquentAccessTokenRepository`
    implementation.
    
    These tests use a real database to validate the entire lifecycle
    of a Sanctum access token, ensuring the persistence layer works
    as expected.
    
    Scenarios covered:
    - `createToken()`: Creation of a new token, including validation
      of its abilities and its persistence in the database.
    - `revokeToken()`: Deletion of an existing token from the database.

---
+++ tests/Feature/Repositories/AccessToken/EloquentAccessTokenRepositoryTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Feature\Repositories\AccessToken;
+
+use App\DTO\AccessToken\CreateAccessTokenDTO;
+use App\Models\User;
+use App\Repositories\AccessToken\EloquentAccessTokenRepository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class EloquentAccessTokenRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_should_create_new_access_token()
+    {
+        $userCreated = User::factory()->create();
+        $name = 'testing';
+        $abilities = ['post:create', 'post:read'];
+        $expiresAt = now()->addHour();
+        $dto = new CreateAccessTokenDTO($userCreated, $name, $abilities, $expiresAt);
+
+        $repository = new EloquentAccessTokenRepository();
+        $newAccessToken = $repository->createToken($dto);
+        $tokenModel = $newAccessToken->accessToken;
+
+        $this->assertDatabaseHas('personal_access_tokens', [
+            'tokenable_type' => User::class,
+            'tokenable_id' => $userCreated->id,
+            'expires_at' => $expiresAt->format('Y-m-d H:i:s'),
+        ]);
+        $this->assertNotEmpty($newAccessToken->plainTextToken);
+
+        $this->assertTrue($tokenModel->can('post:create'));
+        $this->assertTrue($tokenModel->can('post:read'));
+        $this->assertFalse($tokenModel->can('post:delete'));
+    }
+
+    public function test_should_revoke_access_token()
+    {
+        $userCreated = User::factory()->create();
+        $userCreated->createToken('testing');
+        $accessToken = $userCreated->tokens()->first();
+
+        $repository = new EloquentAccessTokenRepository();
+        $result = $repository->revokeToken($accessToken);
+
+        $this->assertDatabaseMissing('personal_access_tokens', [
+            'id' => $accessToken->id,
+        ]);
+        $this->assertTrue($result);
+    }
+}
