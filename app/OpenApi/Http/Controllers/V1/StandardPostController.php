c8e2e1b10da83da42609ffc11e5fc0ed4ddd8f65
Carlos Alexandre <carlos206819@gmail.com>
2025-07-11 12:52:39 -0300

    docs(post): add documentation for public comments endpoint
    This commit introduces the OpenAPI documentation for the public endpoint
    that lists comments for a specific post (`GET /posts/{slug}/comments`).
    
    The annotation details the endpoint's purpose and its possible success
    and error responses, making it explorable via the Swagger UI.

--- app/OpenApi/Http/Controllers/V1/StandardPostController.php
+++ app/OpenApi/Http/Controllers/V1/StandardPostController.php
@@ -101,4 +101,54 @@ class StandardPostController
      * )
      */
     public function show(){}
+
+    /**
+     * @OA\Get(
+     *     path="/v1/posts/{slug}/comments",
+     *     summary="List a Post's Comments",
+     *     description="Retrieves a paginated list of comments for a specific post, identified by its slug.",
+     *     tags={"Public Posts"},
+     *     @OA\Parameter(
+     *          description="The slug of the post whose comments are to be retrieved.",
+     *          in="path",
+     *          name="slug",
+     *          required=true,
+     *          @OA\Schema(type="string"),
+     *      ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="List of the post's comments retrieved successfully.",
+     *         @OA\JsonContent(
+     *             type="object",
+     *              @OA\Property(
+     *                   property="message",
+     *                   type="string",
+     *                   example="Comments for the post retrieved successfully."
+     *               ),
+     *               @OA\Property(
+     *                   property="data",
+     *                   type="array",
+     *                   @OA\Items(ref="#/components/schemas/Comment")
+     *               ),
+     *               @OA\Property(
+     *                   property="links",
+     *                   ref="#/components/schemas/PaginationLinks",
+     *               ),
+     *               @OA\Property(
+     *                   property="meta",
+     *                   ref="#/components/schemas/PaginationMeta",
+     *               ),
+     *         ),
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         ref="#/components/responses/NotFoundRangePage",
+     *     ),
+     *      @OA\Response(
+     *          response="429",
+     *          ref="#/components/responses/TooManyRequests"
+     *      ),
+     * )
+     */
+    public function comments(){}
 }
